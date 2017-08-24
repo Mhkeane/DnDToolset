250270e3f75c1d0922ffdd034544c3710845faf6
Mhkeane <mcraehk@gmail.com>
2017-08-24 16:15:24 +1200

    Initial commit for creation of repo

---
+++ dirCreator.py
@@ -0,0 +1,9 @@
+"""
+This tool is used to create a number of directories, each of which correspond to an index.
+This index represents a hex for the hex map of the region in which my Dungeons and Dragons campaign is set.
+"""
+
+import os
+
+currentDir = os.getcwd()
+print(currentDir)
