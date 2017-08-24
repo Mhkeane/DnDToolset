d59f4d28c5a2de8951128412a6fcc3b8317ce8e8
Mhkeane <mcraehk@gmail.com>
2017-08-24 17:03:31 +1200

    Building a way to specify a range of letters and numbers to create directories across those ranges. Alphabet range currently only works for starting with a single character. There is otherwise support for any start and end number, and multiple character alphabet ends, as well as a lowercase, uppercase, or both, alphabets.

--- dirCreator.py
+++ dirCreator.py
@@ -7,12 +7,47 @@ import os
 import string
 
 workingDir = "C:\\Users\\Blue\\Dropbox\\D&D\\Ashor Wildlands"
-alphabetRange = ["A", "Z"]
-numericRange = [1, 20]
+alphabetRange = ["A", "AZ"]
+numericRange = [1, 10]
 
 # print(os.listdir(workingDir))
 # os.makedirs(os.path.join(workingDir, "Test"))
 # os.rmdir(os.path.join(workingDir, "Test"))
 
+alphabetUsed = string.ascii_uppercase
+# Options for upper or lowercase only, or both,
+# using string.ascii_uppercase, string.ascii_lowercase, or string.ascii_letters respectively
+alphabetListToUse = []
+alphabetStartIndex = alphabetUsed.index(str(alphabetRange[0]))
+
+if len(alphabetRange[1]) > 1:
+    for i in range(len(alphabetRange[1])):
+        if i == 0:
+            for j in range(alphabetStartIndex, len(alphabetUsed) ):
+                alphabetListToUse.append(str(alphabetUsed[i] + alphabetUsed[j]))
+
+        elif i < len(alphabetRange[1]):
+            for j in range(0, len(alphabetUsed)):
+                alphabetListToUse.append(str(alphabetUsed[i] + alphabetUsed[j]))
+
+        else:
+            for j in range(0, alphabetUsed.index(str(alphabetRange[1][len(alphabetRange[1])])) ):
+                alphabetListToUse.append(str(alphabetUsed[i] + alphabetUsed[j]))
 
 
+else:
+    alphabetEndIndex = alphabetUsed.index(str(alphabetRange[1])) + 1
+    for i in range(alphabetStartIndex, alphabetEndIndex):
+        alphabetListToUse.append(str(alphabetUsed[i]))
+
+print(alphabetListToUse)
+
+numericListToUse = [str(i).zfill(len(str(numericRange[1]))) for i in range(numericRange[0], numericRange[1] + 1)]
+
+
+print(numericListToUse)
+"""
+for letter in alphabetListToUse:
+    for number in numericListToUse:
+        os.mkdir(os.path.join(workingDir, letter + number))
+"""
